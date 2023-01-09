9bcbbe683f7ee3c04f0f31e6117f6a1c0d1d7580
JayMoh <86493008+jaycode8@users.noreply.github.com>
2023-01-09 15:18:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ dotenv.config();
 const PORT = process.env.PORT || 4600;
 const roters = require('./Routes/routes');
 const app = express();
+const home = require('./Routes/home')
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(bodyParse.urlencoded({extended:false}));
-app.use('/', roters);
+app.use('/', home);
+
 
 app.listen(PORT, ()=>{
     console.log(`server running at port ${PORT}`);
